fed19905c06448743e1281655f87ba30cc7042b9
ZeroFuku <12mnsin@gmail.com>
2026-07-22 02:10:55 +0800

    finalized

---
+++ Backend/src/main/java/com/zerofuku/auth/demo/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.zerofuku.auth.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UserDTO {
+    private String username;
+    private String email;
+}
